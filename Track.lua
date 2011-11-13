88b77b37eb385fc31f4541b10e43ec7872287662
kbinani <kbinani@9d680d4f-cbfa-477e-9905-73dbafb9b024>
2011-11-13 18:34:08 +0000

    [luavsq] トラックを処理するクラス(現状はカラ)を追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/cadencii/luavsq/trunk@1775 9d680d4f-cbfa-477e-9905-73dbafb9b024

---
+++ Track.lua
@@ -0,0 +1,23 @@
+--[[
+  Track.lua
+  Copyright © 2011 kbinani
+
+  This file is part of luavsq.
+
+  luavsq is free software; you can redistribute it and/or
+  modify it under the terms of the BSD License.
+
+  luavsq is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+]]
+
+if( nil == luavsq )then
+    luavsq = {};
+end
+
+if( nil == luavsq.Track )then
+
+    luavsq.Track = {};
+
+end
