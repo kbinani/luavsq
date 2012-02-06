96d6414488c5567889e47232a5d7bd8cf183241c
kbinani <kbinani@9d680d4f-cbfa-477e-9905-73dbafb9b024>
2012-02-06 17:33:32 +0000

    [luavsq] ヘルパーDLLを更新; VOCALOID3の実行ファイルのバージョンを判定し、UTF-8/ネイティブのいずれの文字列を返すかを自動切替するようにした
    git-svn-id: http://svn.sourceforge.jp/svnroot/cadencii/luavsq/trunk@1980 9d680d4f-cbfa-477e-9905-73dbafb9b024

--- tool/GetSaveFileName/GetSaveFileName.cpp
+++ tool/GetSaveFileName/GetSaveFileName.cpp
@@ -1,6 +1,6 @@
 /**
- * GetSaveFileName.c
- * Copyright © 2011 kbinani
+ * GetSaveFileName.cpp
+ * Copyright © 2011-2012 kbinani
  *
  * This file is part of ExportVSQ.
  *
@@ -13,6 +13,7 @@
  */
 #pragma comment( lib, "lua5.1.lib" )
 #pragma comment( lib, "icuuc.lib" )
+#pragma comment( lib, "version.lib" )
 extern "C"{
     #include <lua.h>
     #include <lualib.h>
@@ -22,6 +23,9 @@ extern "C"{
 #include <tchar.h>
 #include <unicode/ucnv.h>
 #include <vector>
+#include "StringUtil.h"
+
+using namespace std;
 
 void encode( const std::wstring& src, const std::string& codepage, std::string& dst )
 {
@@ -38,64 +42,154 @@ void encode( const std::wstring& src, const std::string& codepage, std::string& 
     }
 }
 
+string getFileVersion( const char *path )
+{
+    string result = "";
+    DWORD dwZero = 0;
+    DWORD dwVerInfoSize = GetFileVersionInfoSizeA( path, &dwZero );
+    if( dwVerInfoSize == 0 ){
+        return result;
+    }
+
+    unsigned char *pBlock = new unsigned char[dwVerInfoSize];
+    if( pBlock == NULL ){
+        return result;
+    }
+    GetFileVersionInfoA( path, dwZero, dwVerInfoSize, pBlock );
+
+    void *pvVersion;
+    UINT VersionLen;
+
+    struct LANGANDCODEPAGE {
+        WORD wLanguage;
+        WORD wCodePage;
+    } *lpTranslate;
+
+    UINT TranslateLen;
+    VerQueryValue(
+        pBlock,
+        TEXT( "\\VarFileInfo\\Translation" ),
+        (LPVOID*)&lpTranslate,
+        &TranslateLen
+    );
+    for( UINT i = 0; i < TranslateLen / sizeof( *lpTranslate ); i++ ){
+        char name[256];
+        sprintf( name, "\\StringFileInfo\\%04x%04x\\ProductVersion",
+                  lpTranslate[i].wLanguage, lpTranslate[i].wCodePage );
+        if( VerQueryValueA( pBlock, name, &pvVersion, &VersionLen ) ){
+            result = (char *)pvVersion;
+            break;
+        }
+    }
+    delete [] pBlock;
+    return result;
+}
+
 /**
- * 「ファイル名を付けて保存」ダイアログボックスを開き、ダイアログで指定されたファイル名を取得する。
- * VOCALOID3 Editor が JobPlugin 起動時に表示する "Running Job Plugin ..." というタイトルのウィンドウを
- * 検索し、そのウィンドウに対してモーダルにダイアログボックスを開こうとする
- *
- * @usage Lua スクリプトからは、以下のように使用する
- * <code>
- *     local luavsq_GetSaveFileName = package.loadlib( "GetSaveFileName.dll", "luavsq_GetSaveFileName" );
- *     if( nil == luavsq_GetSaveFileName )then
- *         assert( false, "DLL のロードエラー" );
- *     end
- *     local path, message = luavsq_GetSaveFileName( "" );
- *     if( nil == path )then
- *         assert( false, "DLL の実行時エラー; エラーメッセージ: " .. message );
- *     else
- *         print( "指定されたファイルパス: " .. path );
- *     end
- * </code>
+ * VOCALOID3 エディタのバージョンから、そのバージョンがUTF8のio.openをサポートするかどうかを判定する
  */
+bool isUtf8Supported( string version )
+{
+    vector<string> versionNumbers = StringUtil::explode( ", ", version );
+    if( versionNumbers.size() >= 4 ){
+        int major = atoi( versionNumbers[0].c_str() );
+        int minor = atoi( versionNumbers[1].c_str() );
+        int release = atoi( versionNumbers[2].c_str() );
+        int build = atoi( versionNumbers[3].c_str() );
+        if( major >= 3 && minor >= 0 && release >= 4 ){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return true;
+    }
+}
 
 extern "C"{
 
+    /**
+     * 「ファイル名を付けて保存」ダイアログボックスを開き、ダイアログで指定されたファイル名を取得する。
+     * VOCALOID3 Editor が JobPlugin 起動時に表示する "Running Job Plugin ..." というタイトルのウィンドウを
+     * 検索し、そのウィンドウに対してモーダルにダイアログボックスを開こうとする
+     *
+     * @usage Lua スクリプトからは、以下のように使用する
+     * <code>
+     *     local luavsq_GetSaveFileName = package.loadlib( "GetSaveFileName.dll", "luavsq_GetSaveFileName" );
+     *     if( nil == luavsq_GetSaveFileName )then
+     *         assert( false, "DLL のロードエラー" );
+     *     end
+     *     local path, message = luavsq_GetSaveFileName( "" );
+     *     if( nil == path )then
+     *         assert( false, "DLL の実行時エラー; エラーメッセージ: " .. message );
+     *     else
+     *         print( "指定されたファイルパス: " .. path );
+     *     end
+     * </code>
+     */
     int luavsq_GetSaveFileName( lua_State *state )
     {
         int n = lua_gettop( state );
 
-        if( n == 1 ){
+        if( n == 2 ){
             const char *path = lua_tostring( state, 1 );
-
-            TCHAR fileNameFull[MAX_PATH] = TEXT( "" );
-            TCHAR fileName[MAX_PATH] = TEXT( "" );
-
+            const char *vocaloid3exe = lua_tostring( state, 2 );
             HWND progressDialog = FindWindow( NULL, TEXT( "Running Job Plugin ..." ) );
 
-            OPENFILENAME arg;
-            ZeroMemory( &arg, sizeof( arg ) );
-            arg.lStructSize = sizeof( arg );
-            arg.hwndOwner = progressDialog;
-            arg.lpstrFilter = TEXT( "VOCALOID2 Sequence (*.vsq)\0*.vsq\0All Files (*.*)\0*.*\0\0" );
-            arg.lpstrFile = fileNameFull;
-            arg.lpstrFileTitle = fileName;
-            arg.nMaxFile = sizeof( fileNameFull );
-            arg.nMaxFileTitle = sizeof( fileName );
-            arg.Flags = OFN_OVERWRITEPROMPT;
-            arg.lpstrTitle = TEXT( "Export VOCALOID2 Sequence" );
-            arg.lpstrDefExt = TEXT( "vsq" );
+            string version = getFileVersion( vocaloid3exe );
 
-            if( GetSaveFileName( &arg ) ){
-                std::wstring fileNameWide = arg.lpstrFile;
-                std::string fileNameUtf8;
-                encode( fileNameWide, "UTF-8", fileNameUtf8 );
-                lua_pushstring( state, fileNameUtf8.c_str() );
-                lua_pushstring( state, "Succeeded." );
+            if( isUtf8Supported( version ) ){
+                WCHAR fileNameFull[MAX_PATH] = L"";
+                WCHAR fileName[MAX_PATH] = L"";
+
+                OPENFILENAMEW arg;
+                ZeroMemory( &arg, sizeof( arg ) );
+                arg.lStructSize = sizeof( arg );
+                arg.hwndOwner = progressDialog;
+                arg.lpstrFilter = L"VOCALOID2 Sequence (*.vsq)\0*.vsq\0All Files (*.*)\0*.*\0\0";
+                arg.lpstrFile = fileNameFull;
+                arg.lpstrFileTitle = fileName;
+                arg.nMaxFile = sizeof( fileNameFull );
+                arg.nMaxFileTitle = sizeof( fileName );
+                arg.Flags = OFN_OVERWRITEPROMPT;
+                arg.lpstrTitle = L"Export VOCALOID2 Sequence";
+                arg.lpstrDefExt = L"vsq";
+
+                if( GetSaveFileNameW( &arg ) ){
+                    std::wstring fileNameWide = arg.lpstrFile;
+                    std::string fileNameUtf8;
+                    encode( fileNameWide, "UTF-8", fileNameUtf8 );
+                    lua_pushstring( state, fileNameUtf8.c_str() );
+                    lua_pushstring( state, "Succeeded." );
+                }else{
+                    lua_pushnil( state );
+                    lua_pushstring( state, "GetSaveFileName WinAPI call return FALSE." );
+                }
             }else{
-                lua_pushnil( state );
-                lua_pushstring( state, "GetSaveFileName WinAPI call return FALSE." );
-            }
+                CHAR fileNameFull[MAX_PATH] = "";
+                CHAR fileName[MAX_PATH] = "";
 
+                OPENFILENAMEA arg;
+                ZeroMemory( &arg, sizeof( arg ) );
+                arg.lStructSize = sizeof( arg );
+                arg.hwndOwner = progressDialog;
+                arg.lpstrFilter = "VOCALOID2 Sequence (*.vsq)\0*.vsq\0All Files (*.*)\0*.*\0\0";
+                arg.lpstrFile = fileNameFull;
+                arg.lpstrFileTitle = fileName;
+                arg.nMaxFile = sizeof( fileNameFull );
+                arg.nMaxFileTitle = sizeof( fileName );
+                arg.Flags = OFN_OVERWRITEPROMPT;
+                arg.lpstrTitle = "Export VOCALOID2 Sequence";
+                arg.lpstrDefExt = "vsq";
+
+                if( GetSaveFileNameA( &arg ) ){
+                    lua_pushstring( state, arg.lpstrFile );
+                    lua_pushstring( state, "Succeeded." );
+                }else{
+                    lua_pushnil( state );
+                    lua_pushstring( state, "GetSaveFileName WinAPI call return FALSE." );
+                }
+            }
         }else{
             lua_pushnil( state );
             lua_pushstring( state, "Invalid number of argument(s)." );
---
+++ tool/GetSaveFileName/StringUtil.h
@@ -0,0 +1,83 @@
+/**
+ * StringUtil.h
+ * Copyright © 2012 kbinani
+ *
+ * This file is part of ExportVSQ.
+ *
+ * ExportVSQ is free software; you can redistribute it and/or
+ * modify it under the terms of the BSD License.
+ *
+ * ExportVSQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#ifndef __StringUtil_h__
+#define __StringUtil_h__
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+ * 文字列関連のユーティリティ
+ */
+class StringUtil
+{
+public:
+    /**
+     * 文字列を区切り文字で分割する
+     * @param delimiter 区切り文字
+     * @param text 文字列
+     * @param limit 区切る回数の最大値
+     * @return 区切られた文字列のリスト
+     */
+    static vector<string> explode( string delimiter, string text, string::size_type limit = string::npos )
+    {
+        vector<string> result;
+        string::size_type searchFrom = 0;
+        string::size_type delimiterIndex = text.find( delimiter, searchFrom );
+        while( delimiterIndex != string::npos ){
+            string token = text.substr( searchFrom, delimiterIndex - searchFrom );
+            result.push_back( token );
+            searchFrom = delimiterIndex + delimiter.length();
+            if( result.size() + 1 == limit ){
+                break;
+            }
+            delimiterIndex = text.find( delimiter, searchFrom );
+        }
+        result.push_back( text.substr( searchFrom ) );
+        return result;
+    }
+
+    /**
+     * 含まれる文字列を全て置換する
+     * @param text 処理対象の文字列
+     * @param search 検索する文字列
+     * @param replace 置換する文字列
+     * @return 置換後の文字列
+     */
+    static string replace( string text, string search, string replace )
+    {
+        if( search == replace ){
+            return text;
+        }
+        string result = text;
+        string::size_type index = result.find( search, 0 );
+        int searchLength = search.length();
+        int replaceLength = replace.length();
+
+        while( string::npos != index ){
+            result.replace( index, searchLength, replace );
+            index = result.find( search, index - searchLength + replaceLength + 1 );
+        }
+        return result;
+    }
+
+private:
+    StringUtil()
+    {
+    }
+};
+
+#endif
